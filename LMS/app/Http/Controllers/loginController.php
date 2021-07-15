751b7075f09093c4f10d223c3021b78ec2d3c783
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-07-15 11:38:23 +0200

    login controller with validation,authorization and redirection

---
+++ LMS/app/Http/Controllers/loginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class loginController extends Controller
+{
+
+    //
+    public function post(Request $request)
+    {
+
+
+        $userAttempt = $request->validate([
+            "email" => "required|email",
+            "password" => "required",
+        ]);
+
+        if (Auth::attempt($userAttempt)) {
+            $request->session()->regenerate();
+            return redirect()->route('home');
+        } else {
+            return back()->withErrors([
+                "email" => "Creditentials do not match",
+            ]);
+        }
+    }
+}
