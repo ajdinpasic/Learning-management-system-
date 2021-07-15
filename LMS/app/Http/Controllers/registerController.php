0869e6b5fe149cf6082d19c910fae4c5aef57f47
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-07-15 10:01:32 +0200

    hashing password

--- LMS/app/Http/Controllers/registerController.php
+++ LMS/app/Http/Controllers/registerController.php
@@ -4,26 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class registerController extends Controller
 {
-    
-    public function post(Request $request) {
-       
-        
-        $newUser=$request->validate([
+
+    public function post(Request $request)
+    {
+
+
+        $newUser = $request->validate([
             'username' => 'required|min:3|max:8|alpha',
             'email' => 'required|email',
             'password' => 'required|min:4|confirmed',
-        ]); 
-        
+        ]);
+
         User::create([
             'name' => $request->username,
-            'email' => $request->email, 
-            'password' => $request->password, 
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
         ]);
-        
-        return redirect()->route('login');
 
+        return redirect()->route('login');
     }
 }
