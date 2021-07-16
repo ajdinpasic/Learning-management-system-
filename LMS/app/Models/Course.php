b760c30286de68487ed429d0f8d9dc2b5bf59661
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-07-16 15:28:56 +0200

    course model and factory

---
+++ LMS/app/Models/Course.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Course extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'ects',
+    ];
+
+    public function course()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
