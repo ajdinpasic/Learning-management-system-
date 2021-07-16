f71aa335a947c018c9100b4487738caf632070a9
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-07-16 16:06:16 +0200

    number of records displayed

--- LMS/app/Http/Controllers/indexController.php
+++ LMS/app/Http/Controllers/indexController.php
@@ -3,13 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Course;
-use Illuminate\Http\Request;
 
 class indexController extends Controller
 {
     public function index()
     {
-        $courses = Course::paginate(10);
+        $courses = Course::paginate(3);
         $total_courses = count($courses);
         return view('layouts.index', ["courses" => $courses, "total_courses" => $total_courses]);
     }
