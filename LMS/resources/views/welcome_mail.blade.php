be7f345a55790fe0a701268edb7603a1101e50a7
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-07-16 17:03:34 +0200

    mail markdown template

---
+++ LMS/resources/views/welcome_mail.blade.php
@@ -0,0 +1,12 @@
+@component('mail::message')
+# Hello {{ auth()->user()->name}}
+
+We are glad You have created account at our webiste. Enjoy !
+
+@component('mail::button', ['url' => 'http://127.0.0.1:8000/login'])
+Proceed by clicking here
+@endcomponent
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
