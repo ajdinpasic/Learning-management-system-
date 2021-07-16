ddbcecafa0ba0b0a24caa704f9da08fe8dffd13c
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-07-16 17:16:49 +0200

    user name instead of profile name

--- LMS/resources/views/layouts/index.blade.php
+++ LMS/resources/views/layouts/index.blade.php
@@ -781,7 +781,7 @@
                             d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
                           ></path>
                         </svg>
-                        <span>Profile</span>
+                        <span>{{ auth()->user()->name}}</span>
                       </a>
                     </li>
                     <li class="flex">
