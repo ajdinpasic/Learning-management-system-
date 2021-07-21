09b02a1028e9d7a5eb50c8ce1877ed69035f646a
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-07-21 10:56:23 +0200

    display records with eloquent rl instead of query builder

--- LMS/app/Http/Controllers/indexController.php
+++ LMS/app/Http/Controllers/indexController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Course;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class indexController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $courses = Course::where("department", "=", "IT")->paginate(2);
+        //$courses = Course::where("department", "=", "IT")->paginate(2);
+        //dd($request->user()->course);
+        $courses = $request->user()->course()->paginate(2);
         $total_courses = Course::where("department", "=", "IT")->count();
         return view('layouts.index', ["courses" => $courses, "total_courses" => $total_courses]);
     }
