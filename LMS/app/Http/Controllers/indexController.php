20c618442aa9245ed63bf539025c81cca246509d
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-08-08 14:55:45 +0200

    counting total exams

--- LMS/app/Http/Controllers/indexController.php
+++ LMS/app/Http/Controllers/indexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Grade;
 use App\Models\Course;
 use Illuminate\Http\Request;
@@ -11,16 +12,17 @@ class indexController extends Controller
 {
     public function index(Request $request)
     {
-        //$courses = Course::where("department", "=", "IT")->paginate(2);
-        //dd($request->user()->course);
-        //$courses = $request->user()->course()->paginate(2);
+
         $courses = $request->user()->course()->select('name', 'ects')->get();
-        //$courses = Course::select('name')->where("department", "=", "IT")->paginate(2);
+
         $total_courses = Course::where("department", "=", "IT")->where("user_id", "=", auth()->user()->id)->count();
 
         $grades = $request->user()->grade()->select('title', 'grade')->get();
         $total_grades = Grade::where("user_id", "=", auth()->user()->id)->count();
 
-        return view('layouts.index', ["courses" => $courses, "total_courses" => $total_courses, "grades" => $grades, "total_grades" => $total_grades]);
+        $allExamDates = $request->user()->examDate()->get(['title', 'scheduled_for']);
+        $total_exams = $request->user()->examDate()->count();
+
+        return view('layouts.index', ["courses" => $courses, "total_courses" => $total_courses, "grades" => $grades, "total_grades" => $total_grades, "allExamDates" => $allExamDates, "total_exams" => $total_exams]);
     }
 }
