c26312cf7a8ef6b5b888f00894bb818927138eb5
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-08-08 12:27:43 +0200

    adding route to index

--- LMS/resources/views/layouts/index.blade.php
+++ LMS/resources/views/layouts/index.blade.php
@@ -213,8 +213,8 @@
                   <li
                     class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
                   >
-                    <a class="w-full" href="">
-                      ?
+                    <a class="w-full" href="{{ route('admin.dates') }}">
+                      Exam dates
                     </a>
                   </li>
                   <li
