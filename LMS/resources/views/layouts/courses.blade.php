21b4de417e882308ce80083280a8e5b9b65fc128
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-08-11 14:41:57 +0200

    removing li from menu

--- LMS/resources/views/layouts/courses.blade.php
+++ LMS/resources/views/layouts/courses.blade.php
@@ -144,18 +144,7 @@
                 <span class="ml-4">Exam dates</span>
               </a>
             </li>
-            <li class="relative px-6 py-3">
-              <a
-                class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                href=""
-              >
-               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar2-plus" viewBox="0 0 16 16">
-  <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2z"/>
-  <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4zM8 8a.5.5 0 0 1 .5.5V10H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V11H6a.5.5 0 0 1 0-1h1.5V8.5A.5.5 0 0 1 8 8z"/>
-</svg>
-                <span class="ml-4">Staff weekly schedule</span>
-              </a>
-            </li>
+            
             @if(Auth::check() && Auth::user()->role == "admin")
             <li class="relative px-6 py-3">
               <button
