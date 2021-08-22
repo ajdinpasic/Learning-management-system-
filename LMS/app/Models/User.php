ac37032cfb5129fc14af213739b3d7c4f2209df8
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-08-22 10:02:27 +0200

    testing new branch

--- LMS/app/Models/User.php
+++ LMS/app/Models/User.php
@@ -55,6 +55,6 @@ class User extends Authenticatable
 
     public function examDate()
     {
-        return $this->hasMany(Exam::class);
+        return $this->hasMany(Exam::class); //test
     }
 }
