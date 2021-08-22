929afb003ba1d3d9f8b51fda4649d6a7f9f26a0c
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-08-22 11:27:38 +0200

    adding fillable to models

--- LMS/app/Models/CourseRegistration.php
+++ LMS/app/Models/CourseRegistration.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class CourseRegistration extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "user_id",
+        "course_id",
+    ];
 }
