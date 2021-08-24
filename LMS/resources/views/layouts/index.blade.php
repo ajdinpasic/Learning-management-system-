737f62b66fe4a341ed6141f205e7d8e468940d0a
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-08-24 18:39:33 +0200

    adding route to menu

--- LMS/resources/views/layouts/index.blade.php
+++ LMS/resources/views/layouts/index.blade.php
@@ -17,17 +17,7 @@
       src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"
       defer
     ></script>
-     <script src="/assets/js/init-alpine.js"></script> 
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css"
-    />
-    <script
-      src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"
-      defer
-    ></script>
-     <script src="/assets/js/charts-lines.js" defer></script> 
-    <script src="/assets/js/charts-pie.js" defer></script> 
+     
       <!-- <style>@yield('css')</style> THIS IS OLD -->
       <link rel="stylesheet" href="http://cdn.bootcss.com/toastr.js/latest/css/toastr.min.css"> 
   </head>
@@ -880,7 +870,7 @@
                   <p
                     class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400"
                   >
-                    Attendance
+                    Total installments
                   </p>
                   <p
                     class="text-lg font-semibold text-gray-700 dark:text-gray-200"
