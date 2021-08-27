d2a8a4ed34b9dfdd10956e69b6084d29194ce4b4
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-08-27 12:07:29 +0200

    fetching attendance with course and adding limit to each query

--- LMS/app/Http/Controllers/indexController.php
+++ LMS/app/Http/Controllers/indexController.php
@@ -13,35 +13,27 @@ class indexController extends Controller
 {
     public function index(Request $request)
     {
-        /*
-        $courses = $request->user()->course()->select('name', 'ects')->get();
 
-        $total_courses = Course::where("department", "=", "IT")->where("user_id", "=", auth()->user()->id)->count();
-
-        $grades = $request->user()->grade()->select('title', 'grade')->get();
-        $total_grades = Grade::where("user_id", "=", auth()->user()->id)->count();
-
-        $allExamDates = $request->user()->examDate()->get(['title', 'scheduled_for']);
-        $total_exams = $request->user()->examDate()->count();
-
-        return view('layouts.index', ["courses" => $courses, "total_courses" => $total_courses, "grades" => $grades, "total_grades" => $total_grades, "allExamDates" => $allExamDates, "total_exams" => $total_exams]); */
         $user_id = $request->user()->id;
 
-        $totalPoints = DB::select("SELECT courses.name, SUM(grades.student_grade) as suma FROM courses JOIN grades ON courses.id = grades.course_id WHERE grades.user_id = '$user_id' GROUP BY grades.course_id ORDER BY courses.name ASC");
+        $totalPoints = DB::select("SELECT courses.name, SUM(grades.student_grade) as suma FROM courses JOIN grades ON courses.id = grades.course_id WHERE grades.user_id = '$user_id' GROUP BY grades.course_id ORDER BY courses.name ASC LIMIT 2");
+
+        $totalAttendance = DB::select("SELECT courses.name, SUM(user_attendances.present) AS prisustvo FROM user_attendances JOIN courses ON courses.id = user_attendances.course_id WHERE user_attendances.user_id = '$user_id' AND user_attendances.present = 1 GROUP BY user_attendances.course_id LIMIT 2");
+
 
         $allExams = DB::select("SELECT  exams.scheduled_for,courses.name FROM exams 
             JOIN courses ON exams.course_id = courses.id
             JOIN course_registrations ON courses.id = course_registrations.course_id
-            WHERE course_registrations.user_id = '$user_id '
+            WHERE course_registrations.user_id = '$user_id ' LIMIT 2
             ");
 
-        $allGrades = DB::select("SELECT  courses.name,grades.student_grade,grades.examination FROM grades JOIN courses ON courses.id = grades.course_id  WHERE grades.user_id = '$user_id'  ORDER BY courses.name ASC");
+        $allGrades = DB::select("SELECT  courses.name,grades.student_grade,grades.examination FROM grades JOIN courses ON courses.id = grades.course_id  WHERE grades.user_id = '$user_id'  ORDER BY courses.name ASC LIMIT 2");
 
         $numberOfExams = count($allExams);
         $numberOfCourses = count($request->user()->registration);
         $numberOfGrades = count($allGrades);
 
 
-        return view('layouts.index', ["totalPoints" => $totalPoints, "numberOfCourses" => $numberOfCourses, "numberOfExams" => $numberOfExams, "allGrades" => $allGrades, "allExams" => $allExams, "numberOfGrades" => $numberOfGrades]);
+        return view('layouts.index', ["totalPoints" => $totalPoints, "numberOfCourses" => $numberOfCourses, "numberOfExams" => $numberOfExams, "allGrades" => $allGrades, "allExams" => $allExams, "numberOfGrades" => $numberOfGrades, "totalAttendance" => $totalAttendance]);
     }
 }
