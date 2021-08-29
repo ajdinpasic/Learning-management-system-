0d07cf8cbae06186b00309d9397dec1dcf1a7b8c
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-08-29 23:18:54 +0200

    adding fillable to exam

--- LMS/app/Models/Exam.php
+++ LMS/app/Models/Exam.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Exam extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'scheduled_for',
+        'classroom_number',
+        'duration_time',
+        'proctor',
+        'course_id',
+        'title',
+    ];
 }
