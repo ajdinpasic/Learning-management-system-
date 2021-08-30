a90a7c777c2d1da21f62509b610c7a7f98c5af3d
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-08-30 16:38:51 +0200

    grade fillable

--- LMS/app/Models/Grade.php
+++ LMS/app/Models/Grade.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Grade extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "max_grade",
+        "student_grade",
+        "examination",
+        "user_id",
+        "course_id",
+    ];
 }
