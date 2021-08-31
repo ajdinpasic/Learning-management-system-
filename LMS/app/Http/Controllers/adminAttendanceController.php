63213267be3e32029d5ddc05e0498d3874d2aa2a
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-08-31 18:50:00 +0200

    validating and creating attendance object

--- LMS/app/Http/Controllers/adminAttendanceController.php
+++ LMS/app/Http/Controllers/adminAttendanceController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Course;
 use Illuminate\Http\Request;
+use App\Models\UserAttendance;
 use Illuminate\Support\Facades\DB;
+use Brian2694\Toastr\Facades\Toastr;
 
 class adminAttendanceController extends Controller
 {
@@ -12,5 +15,37 @@ class adminAttendanceController extends Controller
     {
         $allStudents = DB::table('users')->join('course_registrations', 'course_registrations.user_id', '=', 'users.id')->join('courses', 'course_registrations.course_id', '=', 'courses.id')->select('users.name', 'users.surname', 'users.avatar', 'course_registrations.created_at')->where('course_registrations.course_id', $course->id)->paginate(10);
         return view('layouts.admin_attendances', ["allStudents" => $allStudents, "course" => $course]);
+    }
+
+    public function enterAtt(User $user, $course)
+    {
+        return view('actions.action_Enteratt', ["course" => $course, "user" => $user]);
+    }
+
+    public function postAtt(User $user, Request $request)
+    {
+
+
+        $request->validate([
+
+            'attendance' => 'required',
+            'present' =>  'required',
+
+
+        ]);
+
+        $course = Course::where("name", $request->hiddenValueCourse)->first();
+
+        UserAttendance::create([
+
+            "type" => $request->attendance,
+            "present" => $request->present,
+            "user_id" => $user->id,
+            "course_id" => $course->id,
+        ]);
+
+        Toastr::success("Attendance is entered into the system!");
+
+        return back();
     }
 }
