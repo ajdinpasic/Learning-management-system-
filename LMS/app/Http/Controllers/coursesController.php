1007f7f44df17a94c5b74826dca786d086e45a87
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-09-01 10:10:07 +0200

    fetching admin courses

--- LMS/app/Http/Controllers/coursesController.php
+++ LMS/app/Http/Controllers/coursesController.php
@@ -15,6 +15,8 @@ class coursesController extends Controller
 
         $allCourses = DB::select("SELECT course_registrations.final,courses.name,courses.abbreviation,courses.ECTS FROM courses JOIN course_registrations ON courses.id = course_registrations.course_id WHERE course_registrations.user_id = '$user_id' ");
 
-        return view('layouts.courses', ["allCourses" => $allCourses]);
+        $adminCourses = DB::select("SELECT courses.name FROM courses JOIN course_registrations ON course_registrations.course_id = courses.id JOIN users ON users.id = course_registrations.user_id WHERE course_registrations.user_id = '$user_id' ");
+
+        return view('layouts.courses', ["allCourses" => $allCourses, "adminCourses" => $adminCourses]);
     }
 }
