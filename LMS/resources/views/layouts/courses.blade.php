71ba9138469aff657e36c308e8c947bfdd2b63d6
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-09-01 23:06:02 +0200

    listing admin courses

--- LMS/resources/views/layouts/courses.blade.php
+++ LMS/resources/views/layouts/courses.blade.php
@@ -211,7 +211,7 @@
                   <li
                     class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
                   >
-                    <a class="w-full" href="{{route('admin.courses',$course->name)}}">
+                    <a class="w-full" href="{{route('admin.finals',$course->name)}}">
                       {{$course->name}}
                     </a>
                   </li>
