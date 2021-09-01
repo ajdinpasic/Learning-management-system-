90e8a2e2d189797d03f4679d12c61b10c3960b37
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-09-01 10:08:43 +0200

    redirecting after clicking on delete button

--- LMS/resources/views/layouts/admin_attendances.blade.php
+++ LMS/resources/views/layouts/admin_attendances.blade.php
@@ -552,7 +552,7 @@
                           </button>
                           <button
                             class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
-                            aria-label="Delete"  onclick="window.open('http://127.0.0.1:8000/admins/{{$user->name}}/delete_grades/{{$course->name}}')" 
+                            aria-label="Delete"  onclick="window.open('http://127.0.0.1:8000/admins/{{$user->name}}/delete_att/{{$course->name}}')" 
                           >
                             <svg
                               class="w-5 h-5"
